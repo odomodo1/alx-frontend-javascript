cf63dabbc089116cf32fe5e8afd80a090b1f460b
odomodo1 <odomodo19@gmail.com>
2023-09-28 20:18:52 +0000

    One simply does not use async/await without knowing promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -11,5 +11,4 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       arr.push({ status: item.status, value: item.value || item.reason });
     }
     return arr;
-  });
-}
+  });}
